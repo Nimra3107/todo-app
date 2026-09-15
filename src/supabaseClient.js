5cf7956bdf24c2a50663b004cb516f47399a9896
Nimra3107 <aynag5934@example.com>
2026-09-15 18:43:20 +0500

    your changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "https://fzggyzywqgzkoxefbmmk.supabase.co";
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Z2d5enl3cWd6a294ZWZibW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxMjQyNzEsImV4cCI6MjEwNDcwMDI3MX0.nwszWzX0FIMJKkWyD0ImMk2hhgfFa5ZwV7Nv0svVXo0";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
